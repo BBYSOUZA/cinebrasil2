0f94528daaf25480f870c063a93f5a0a8573a0bd
BBYSOUZA <laahyukie@gmail.com>
2025-04-05 09:06:46 +0200

    commit

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,11 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use App\Http\Controllers\SponsorController;
-
-Route::post('/send-sponsor-request', [SponsorController::class, 'sendEmail'])->name('sponsor.send');
-
 abstract class Controller
 {
     //
